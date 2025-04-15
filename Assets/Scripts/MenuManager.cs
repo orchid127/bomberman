402f977756afc7a2e2b1def669ade13b9ed0856c
Kelvish <kelvish.duval@efrei.net>
2025-04-15 13:54:32 +0200

    Création du menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadGame(){
+        SceneManager.LoadScene("PremierNiveau");
     }
 }
