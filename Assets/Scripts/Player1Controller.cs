17993bfee5c5d7b5b6b105c7dc4728013b1c62fe
Kelvish <kelvish.duval@efrei.net>
2025-04-16 14:16:37 +0200

    Ca marche

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -7,6 +7,8 @@ public class Player1Controller : MonoBehaviour
     public Rigidbody rigid;
     public GameObject bombP;
     public int bombRes = 2;
+    public GameObject bombExplose;
+    public float rayon;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -45,6 +47,18 @@ public class Player1Controller : MonoBehaviour
         GameObject bomb = Instantiate(bombP,this.transform.position,bombP.transform.rotation);
         bombRes--;
         yield return new WaitForSeconds(2f);
+        Vector3 spawnPosz = new Vector3(bomb.transform.position.x + rayon,bomb.transform.position.y, bomb.transform.position.z);
+        Vector3 spawnPoss = new Vector3(bomb.transform.position.x - rayon,bomb.transform.position.y, bomb.transform.position.z); 
+        Vector3 spawnPosq = new Vector3(bomb.transform.position.x,bomb.transform.position.y, bomb.transform.position.z - rayon); 
+        Vector3 spawnPosd = new Vector3(bomb.transform.position.x,bomb.transform.position.y, bomb.transform.position.z + rayon);  
+        GameObject haut = Instantiate(bombExplose, spawnPosz, bombExplose.transform.rotation);
+        GameObject bas = Instantiate(bombExplose, spawnPoss, bombExplose.transform.rotation);
+        GameObject gauche = Instantiate(bombExplose, spawnPosq, bombExplose.transform.rotation);
+        GameObject droite = Instantiate(bombExplose, spawnPosd, bombExplose.transform.rotation);
+        Destroy(haut,1f);
+        Destroy(bas,1f);
+        Destroy(gauche,1f);
+        Destroy(droite);        
         Destroy(bomb);
         bombRes++;
     }
