9b70a9c7aeceacf138142dc4e42c3e827c12f101
Kelvish <kelvish.duval@efrei.net>
2025-04-16 10:23:11 +0200

    Bombes qui marchent

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -6,8 +6,6 @@ public class BombController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(this.gameObject,2f);
-        Instantiate(bombExplose, this.transform.position, bombExplose.transform.rotation);
     }
 
     // Update is called once per frame
